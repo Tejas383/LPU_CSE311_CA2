6debb8e708d196cbcaaf65e2a0ae71fc42c3cbce
Tejasvita <ttejasvita@gmail.com>
2025-08-30 12:02:01 +0530

    added the priority in results table

--- src/ResultsTable.jsx
+++ src/ResultsTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Table,
   TableBody,
@@ -6,46 +6,73 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 
-const ResultsTable = ({process, setProcess}) => {
+const ResultsTable = ({ process, setProcess }) => {
   return (
-    <div className=' flex flex-col items-center justify-center bg-red-500/50 w-full h-full '>
-      <Table className=''>
+    <div className=" flex flex-col items-center justify-center bg-red-500/50 w-full h-full ">
+      <Table className="">
         <TableHeader>
-          <TableRow className=''>
-            <TableHead className='border border-white text-center'>Process ID</TableHead>
-            <TableHead className='border border-white text-center'>Arrival Time</TableHead>
-            <TableHead className='border border-white text-center'>Burst Time</TableHead>
-            <TableHead className='border border-white text-center'>Completion Time</TableHead>
-            <TableHead className='border border-white text-center'>Turn Around Time</TableHead>
-            <TableHead className='border border-white text-center'>Waiting Time</TableHead>
-            <TableHead className='border border-white text-center'>Response Time</TableHead>
+          <TableRow className="">
+            <TableHead className="border border-white text-center">
+              Process ID
+            </TableHead>
+            <TableHead className="border border-white text-center">
+              Priority
+            </TableHead>
+            <TableHead className="border border-white text-center">
+              Arrival Time
+            </TableHead>
+            <TableHead className="border border-white text-center">
+              Burst Time
+            </TableHead>
+            <TableHead className="border border-white text-center">
+              Completion Time
+            </TableHead>
+            <TableHead className="border border-white text-center">
+              Turn Around Time
+            </TableHead>
+            <TableHead className="border border-white text-center">
+              Waiting Time
+            </TableHead>
+            <TableHead className="border border-white text-center">
+              Response Time
+            </TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {process && process.length > 0 ? (
-              process.map((p, idx) => (
-                <TableRow key={idx}>
-                  <TableCell className='border border-white'>{p.pid}</TableCell>
-                  <TableCell className='border border-white'>{p.at}</TableCell>
-                  <TableCell className='border border-white'>{p.bt}</TableCell>
-                  <TableCell className='border border-white'>{p.ct ?? "-"}</TableCell>
-                  <TableCell className='border border-white'>{p.tat ?? "-"}</TableCell>
-                  <TableCell className='border border-white'>{p.wt ?? "-"}</TableCell>
-                  <TableCell className='border border-white'>{p.rt ?? "-"}</TableCell>
-                </TableRow>
-              ))
+            process.map((p, idx) => (
+              <TableRow key={idx}>
+                <TableCell className="border border-white">{p.pid}</TableCell>
+                <TableCell className="border border-white">{p.pr}</TableCell>
+                <TableCell className="border border-white">{p.at}</TableCell>
+                <TableCell className="border border-white">{p.bt}</TableCell>
+                <TableCell className="border border-white">
+                  {p.ct ?? "-"}
+                </TableCell>
+                <TableCell className="border border-white">
+                  {p.tat ?? "-"}
+                </TableCell>
+                <TableCell className="border border-white">
+                  {p.wt ?? "-"}
+                </TableCell>
+                <TableCell className="border border-white">
+                  {p.rt ?? "-"}
+                </TableCell>
+              </TableRow>
+            ))
           ) : (
             <TableRow>
-                <TableCell colSpan={7} className="text-center">No processes yet</TableCell>
+              <TableCell colSpan={7} className="text-center">
+                No processes yet
+              </TableCell>
             </TableRow>
-          )
-        }
+          )}
         </TableBody>
       </Table>
     </div>
-  )
-}
+  );
+};
 
-export default ResultsTable
+export default ResultsTable;
